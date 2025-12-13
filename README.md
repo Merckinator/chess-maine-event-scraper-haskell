d5a8713df189ee051125d52247f7405599afc699
Jeff Merckens <me@jeffmerckens.com>
2025-12-13 17:17:30 -0500

    initial fetch; start parsing out event titles

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # chess-maine-event-scraper-haskell
+
+Recently was reading "Get Programming in Haskell" by Will Kurt and was wanting to try applying what I was learning. My chess-maine-event-scraper hasn't been working for quite some time now (think they made changes to their website), so this seemed like a good enough project to play with.
+
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main (main) where
 
-import Lib
+import Network.HTTP.Simple (httpLBS, parseRequest, getResponseBody)
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Text.HTML.TagSoup (Tag(TagOpen), parseTags, innerText, (~/=))
+
+fromEvents :: [Tag String] -> String
+fromEvents = innerText . take 2 . dropWhile (~/= TagOpen ("h3" :: String) [("class","entry-header")])
 
 main :: IO ()
-main = someFunc
+main = do
+    req <- parseRequest "GET https://chessmaine.net/chessmaine/events/"
+    resp <- httpLBS req
+    let body = L8.unpack $ getResponseBody resp
+        tags = parseTags body
+        eventTitle = fromEvents tags
+    putStrLn eventTitle
+
